ec19f46e0ace61f54bc0dffb3bf6169efe0eac78
Imtiyaz <imtiyaazkhan@gmail.com>
2026-09-22 08:46:20 +0000

    Code added

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 function login($username, $password)
 {
-    $query = "SELECT  FROM users 
+    $query = "SELECT * FROM users 
               WHERE username = '$username' 
               AND password = '$password'";
 
     $result = mysqli_query($connection, $query);
 
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows(result) > 0) {
         return [
             "success" => true,
             "message" => "Welcome " . $username
